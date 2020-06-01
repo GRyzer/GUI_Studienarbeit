b7d950e4c69d70e7adf789363149dcea745e0f65
GRyzer <GRyzer59@gmail.com>
2020-06-01 14:41:01 +0200

    Added validators for username and password in SignUpPage

---
+++ validators.py
@@ -0,0 +1,21 @@
+from PyQt5.QtGui import QValidator
+
+
+class UsernameValidator(QValidator):
+    def __init__(self):
+        super(UsernameValidator, self).__init__()
+
+    def validate(self, p_str, p_int):
+        if p_int < 5:
+            return QValidator.Intermediate, p_str, p_int
+        return QValidator.Acceptable, p_str, p_int
+
+
+class PasswordValidator(QValidator):
+    def __init__(self):
+        super(PasswordValidator, self).__init__()
+
+    def validate(self, p_str, p_int):
+        if p_int < 8:
+            return QValidator.Intermediate, p_str, p_int
+        return QValidator.Acceptable, p_str, p_int
