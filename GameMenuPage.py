67792118c909b7d64a7a4336e03d0f6da5c97457
GRyzer <GRyzer59@gmail.com>
2020-06-11 18:42:51 +0200

    changed the games buttons

--- GameMenuPage.py
+++ GameMenuPage.py
@@ -11,71 +11,63 @@ class FormWidget(FormWidgetIF):
         self.game_button_storage = []
         self.verticalLayout = None
         self.horizontalLayout = None
-        self.gridLayout2 = None
+        self.username_layout = None
+        self.username_label = None
         self.gridLayout = None
         self.heading = None
         self.main_menu_button = None
-        self.username_label = None
-        self.game1_button = None
-        self.game2_button = None
-        self.game3_button = None
-        self.game4_button = None
 
     def setupUi(self, game_menu_page, username):
         game_menu_page.setMinimumSize(self.get_min_widget())
         game_menu_page.setMaximumSize(self.get_max_widget())
-        game_menu_page.setWindowTitle("Login")
+        game_menu_page.setWindowTitle("Game Menu")
         game_menu_page.resize(self.get_default_window_size())
 
         self.verticalLayout = QVBoxLayout(game_menu_page)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
 
-        self.gridLayout = QtWidgets.QGridLayout(game_menu_page)
-
-        self.heading = QtWidgets.QLabel('Game Menu', game_menu_page)
-        self.heading.setFont(QFont('', 20))
-        self.heading.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
-        self.heading.setWordWrap(True)
-        self.gridLayout.addWidget(self.heading, 2, 0, 1, 1)
-
-        self.horizontalLayout = QtWidgets.QHBoxLayout(game_menu_page)
-        self.horizontalLayout.setContentsMargins(-1, 100, -1, -1)
-
-        self.main_menu_button = QtWidgets.QPushButton('Main Menu', game_menu_page)
-        self.main_menu_button.setSizePolicy(self.get_size_policy(self.main_menu_button))
-
-        self.horizontalLayout.addWidget(self.main_menu_button)
-        self.gridLayout.addLayout(self.horizontalLayout, 4, 0, 1, 1)
+        self.username_layout = QtWidgets.QFormLayout(game_menu_page)
+        self.username_layout.setSizeConstraint(QtWidgets.QLayout.SetMinimumSize)
+        self.username_layout.setLabelAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+        self.username_layout.setFormAlignment(QtCore.Qt.AlignCenter)
+        self.username_layout.setContentsMargins(-1, 20, 40, -1)
 
         self.username_label = QtWidgets.QLabel(username, game_menu_page)
-        self.username_label.setSizePolicy(self.get_size_policy(self.username_label, QSizePolicy.Minimum,
-                                                               QSizePolicy.Minimum))
         self.username_label.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTop | QtCore.Qt.AlignTrailing)
-        self.gridLayout.addWidget(self.username_label, 0, 0, 1, 1)
+        self.username_layout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.username_label)
+        self.verticalLayout.addLayout(self.username_layout)
 
-        self.gridLayout2 = QtWidgets.QGridLayout(game_menu_page)
-        self.gridLayout2.setContentsMargins(20, -1, 20, -1)
-        self.gridLayout2.setHorizontalSpacing(20)
-        self.gridLayout2.setVerticalSpacing(10)
+        self.heading = QtWidgets.QLabel('Game Menu', game_menu_page)
+        self.heading.setSizePolicy(self.get_size_policy(self.heading, QSizePolicy.Minimum, QSizePolicy.Expanding))
+        self.heading.setFont(QFont('', 26))
+        self.heading.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
+        self.verticalLayout.addWidget(self.heading)
 
-        self.game1_button = QtWidgets.QPushButton('Game 1', game_menu_page)
-        self.game_button_storage.append(self.game1_button)
-        self.gridLayout2.addWidget(self.game1_button, 0, 0, 1, 1)
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setSizeConstraint(QtWidgets.QLayout.SetMaximumSize)
 
-        self.game2_button = QtWidgets.QPushButton('Game 2', game_menu_page)
-        self.game_button_storage.append(self.game2_button)
-        self.gridLayout2.addWidget(self.game2_button, 0, 1, 1, 1)
+        for row in range(0, 2):
+            for column in range(1, 3):
+                button = QPushButton(f"Game {column + row * 2}", game_menu_page)
+                button.setSizePolicy(self.get_size_policy(button, QSizePolicy.Fixed, QSizePolicy.Maximum))
+                button.setFont(QFont('', 14))
 
-        self.game3_button = QtWidgets.QPushButton('Game 3', game_menu_page)
-        self.game_button_storage.append(self.game3_button)
-        self.gridLayout2.addWidget(self.game3_button, 1, 0, 1, 1)
+                self.game_button_storage.append(button)
+                self.gridLayout.addWidget(button, row, column)
 
-        self.game4_button = QtWidgets.QPushButton('Game 4', game_menu_page)
-        self.game_button_storage.append(self.game4_button)
-        self.gridLayout2.addWidget(self.game4_button, 1, 1, 1, 1)
-
-        self.gridLayout.addLayout(self.gridLayout2, 3, 0, 1, 1)
         self.verticalLayout.addLayout(self.gridLayout)
+        spacer_up = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacer_up)
+
+        self.horizontalLayout = QHBoxLayout(game_menu_page)
+        self.horizontalLayout.setContentsMargins(-1, -1, -1, 10)
+
+        self.main_menu_button = QPushButton('return to main menu', game_menu_page)
+        self.main_menu_button.setSizePolicy(self.get_size_policy(self.main_menu_button))
+        self.main_menu_button.setFont(QFont('', 12))
+        self.horizontalLayout.addWidget(self.main_menu_button)
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
 
         QtCore.QMetaObject.connectSlotsByName(game_menu_page)
 
