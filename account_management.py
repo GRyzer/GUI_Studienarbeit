a77b9a2af2b62fb9184cb24d36aa4ffa5a540103
GRyzer <GRyzer59@gmail.com>
2020-06-30 22:14:03 +0200

    refactoring

--- account_management.py
+++ account_management.py
@@ -6,11 +6,10 @@ class AccountManagement:
     header = ["username", "password", "age", "gender"]
 
     def __init__(self):
-        self.file_name = "databases/userdatabase.csv"
-        self.df = self.from_file()
-        pass
+        self.file_name = "databases/accounts_database.csv"
+        self.df = self.get_accounts()
 
-    def from_file(self):
+    def get_accounts(self):
         if os.path.exists(self.file_name):
             df = pd.read_csv(self.file_name, header=0)
         else:
@@ -23,13 +22,13 @@ class AccountManagement:
             return True
         return False
 
-    def add_account(self, things: list):
-        if self.is_username_available(things[0]):
-            df2 = pd.DataFrame([things], columns=self.header)
-            df2.to_csv(self.file_name, mode='a', header=False, index=False)
-            self.df = self.df.append(df2, ignore_index=True)
+    def add_account(self, user_information: list):
+        if self.is_username_available(user_information[0]):
+            new_df = pd.DataFrame([user_information], columns=self.header)
+            new_df.to_csv(self.file_name, mode='a', header=False, index=False)
+            self.df = self.df.append(new_df, ignore_index=True)
         else:
-            raise ValueError(f'Username: {things[0]} is already taken!')
+            raise ValueError(f'Username: {user_information[0]} is already taken!')
 
     def check_credentials(self, username, password):
         user_account = self.get_user(username)
